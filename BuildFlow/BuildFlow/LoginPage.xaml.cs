e40a070e67a2459a26bd50e54bf22c6f972d36d0
David Reinhardt <davidreinhardt99@gmail.com>
2022-09-30 20:44:21 -0400

    feat: add model classes, pages, and homeview. Add basic login functionality

---
+++ BuildFlow/BuildFlow/LoginPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BuildFlow
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            //TODO: Uncomment before publishing
+            //bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            //bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            //if (isEmailEmpty || isPasswordEmpty)
+            //{
+                //TODO: Add error code popup for login
+            //}
+            //else
+            //{
+            //    Navigation.PushAsync(new HomePage());
+            //}
+
+            Navigation.PushAsync(new HomePage());
+        }
+    }
+}
