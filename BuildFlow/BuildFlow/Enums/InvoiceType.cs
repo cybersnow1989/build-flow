4029b087e5bab157fdf3e2daa93ade92c0537fa4
dreinhardt <cybersnow1989@gmail.com>
2022-10-21 09:39:42 -0400

    feat: fix CustomerDetailsViewModel and update models

---
+++ BuildFlow/BuildFlow/Enums/InvoiceType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildFlow.Enums
+{
+    public enum InvoiceType
+    {
+        Regular,
+        Estimate
+    }
+}
